95f24d0eb229d99b736f6d157e6f2c93b8a9d465
hungrybeagle <cyang@deltalearns.ca>
2022-03-31 08:25:05 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,9 +7,9 @@ def sum(a,b):
     # return value: returns the sum of the 2 numbers
 
 
+if __name__ == "__main__":
+    #this should return a value of 7
+    x = sum(3,4)
 
-#this should return a value of 7
-x = sum(3,4)
-
-#this should return a value of 12.5
-y = sum(11,1.5)
+    #this should return a value of 12.5
+    y = sum(11,1.5)
